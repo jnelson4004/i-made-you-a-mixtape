7fdc918cdca8f461bfa42b7f2d7e7d6549e4f94b
Erin <50000677+sinclairems@users.noreply.github.com>
2024-02-08 21:45:11 -0600

    Everything works on my end with this update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Image Shuffler API
 
 
@@ -21,3 +22,50 @@ function sound(){
 // .catch(err => {
 // console.error(err);
 // });
+=======
+function soundOne() {
+    var snd = new Audio("assets/songs/blink-182 - All The Small Things (Official Music Video).mp3")//wav is also supported
+    snd.play()//plays the sound
+}
+
+function soundTwo() {
+    var snd = new Audio("assets/songs/Elliott Smith - Say Yes (from EitherOr).mp3")//wav is also supported
+    snd.play()//plays the sound
+}
+
+function soundThree() {
+    var snd = new Audio("assets/songs/Goo Goo Dolls  Iris [Official Music Video] [4K Remaster].mp3")//wav is also supported
+    snd.play()//plays the sound
+}
+
+function soundFour() {
+    var snd = new Audio("assets/songs/I'll Stand by You.mp3")//wav is also supported
+    snd.play()//plays the sound
+}
+
+function soundFive() {
+    var snd = new Audio("assets/songs/The Replacements - Cant Hardly Wait (Official Music Video).mp3")//wav is also supported
+    snd.play()//plays the sound
+}
+
+function soundSix() {
+    var snd = new Audio("assets/songs/Lisa Loeb _Stay (I Missed You)_ Music Video.mp3")//wav is also supported
+    snd.play()//plays the sound
+}
+
+function soundSeven() {
+    var snd = new Audio("assets/songs/Smash Mouth - All Star (Official Music Video).mp3")//wav is also supported
+    snd.play()//plays the sound
+}
+
+function soundEight() {
+    var snd = new Audio("assets/songs/Haddaway - What Is Love [Official].mp3")//wav is also supported
+    snd.play()//plays the sound
+}
+
+function soundNine() {
+    var snd = new Audio("assets/songs/Foo Fighters - Everlong.mp3")//wav is also supported
+    snd.play()//plays the sound
+}
+
+>>>>>>> Stashed changes
