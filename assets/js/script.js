6dc2f3640b291c3ea03bc2f0f6ba4842f3326acc
Erin <50000677+sinclairems@users.noreply.github.com>
2024-02-08 21:22:33 -0600

    Love Quiz JS and an html form

---
+++ assets/js/script.js
@@ -0,0 +1,75 @@
+// Music Player
+function soundOne() {
+    var snd = new Audio("assets/songs/blink-182 - All The Small Things (Official Music Video).mp3")//wav is also supported
+    snd.play()//plays the sound
+}
+
+function soundTwo() {
+    var snd = new Audio("assets/songs/Elliott Smith - Say Yes (from EitherOr).mp3")//wav is also supported
+    snd.play()//plays the sound
+}
+
+function soundThree() {
+    var snd = new Audio("assets/songs/Goo Goo Dolls  Iris [Official Music Video] [4K Remaster].mp3")//wav is also supported
+    snd.play()//plays the sound
+}
+
+function soundFour() {
+    var snd = new Audio("assets/songs/I'll Stand by You.mp3")//wav is also supported
+    snd.play()//plays the sound
+}
+
+function soundFive() {
+    var snd = new Audio("assets/songs/The Replacements - Cant Hardly Wait (Official Music Video).mp3")//wav is also supported
+    snd.play()//plays the sound
+}
+
+function soundSix() {
+    var snd = new Audio("assets/songs/Lisa Loeb _Stay (I Missed You)_ Music Video.mp3")//wav is also supported
+    snd.play()//plays the sound
+}
+
+function soundSeven() {
+    var snd = new Audio("assets/songs/Smash Mouth - All Star (Official Music Video).mp3")//wav is also supported
+    snd.play()//plays the sound
+}
+
+function soundEight() {
+    var snd = new Audio("assets/songs/Haddaway - What Is Love [Official].mp3")//wav is also supported
+    snd.play()//plays the sound
+}
+
+function soundNine() {
+    var snd = new Audio("assets/songs/Foo Fighters - Everlong.mp3")//wav is also supported
+    snd.play()//plays the sound
+}
+
+
+
+// // Love Quiz API
+// function loveQuiz() {
+// var firstName = document.getElementById("firstName");
+// var secondName = document.getElementById("secondName");
+// var calculateBtn = document.getElementById("calculate");
+// var results = document.getElementById("results");
+
+// const url = "https://love-calculator.p.rapidapi.com/getPercentage?sname=" + firstName + "&fname=" + secondName;
+
+// const options = {
+// 	    method: 'GET',
+// 	    headers: {
+// 		    'X-RapidAPI-Key': '618c1f6eb0msh893d6f2ef05e110p19e3f9jsnb4175e2003ee',
+// 		    'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com'
+// 	    }
+//     };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+
+// Image API
